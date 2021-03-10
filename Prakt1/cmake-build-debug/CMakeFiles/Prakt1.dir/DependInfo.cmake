a3a08a6de777dc3595e392d0706e02c4d99aea27
Yani Drenchev <y.drenchev@imeniu.com>
2021-03-10 12:52:26 +0200

    Some tasks solved
    First and second task from:
    
    https://github.com/semerdzhiev/oop-2020-21/blob/main/inf-4-practical/01-structs/problems.md

--- Prakt1/cmake-build-debug/CMakeFiles/Prakt1.dir/DependInfo.cmake
+++ Prakt1/cmake-build-debug/CMakeFiles/Prakt1.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/Users/yanidrenchev/Desktop/UNI/OOP/Prakt1/main.cpp" "/Users/yanidrenchev/Desktop/UNI/OOP/Prakt1/cmake-build-debug/CMakeFiles/Prakt1.dir/main.cpp.o"
+  "/Users/yanidrenchev/Desktop/UNI/OOP/Object-oriented-programming-cpp-summer-2021/Prakt1/main.cpp" "/Users/yanidrenchev/Desktop/UNI/OOP/Object-oriented-programming-cpp-summer-2021/Prakt1/cmake-build-debug/CMakeFiles/Prakt1.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
--- Prakt1/main.cpp
+++ Prakt1/main.cpp
@@ -1,51 +1,83 @@
 #include <iostream>
-
+//FIRST TASK
+//Да се дефина структурата Vector, която определя вектор в
+// тримерното пространство и функциите за: създаване и извеждане на вектор,
+// намиране на дължина на вектор, сума и разлика на два вектора, произведение на вектор с число, нормализация на вектор.
 struct vector {
 public:
     double a, b, c;
 
 };
 
-void create(vector& vector1) {
+void create(vector &vector1) {
     std::cin >> vector1.a;
     std::cin >> vector1.b;
     std::cin >> vector1.c;
 };
 
-void print (const vector& v){
-    std::cout << "a: " << v.a << " b: "<< v.b << " c: " << v.c << "\n";
+void print(const vector &v) {
+    std::cout << "a: " << v.a << " b: " << v.b << " c: " << v.c << "\n";
 };
-double len (const vector& v){
-    double len = sqrt(v.a * v.a + v.b*v.b + v.c*v.c);
+
+double len(const vector &v) {
+    double len = sqrt(v.a * v.a + v.b * v.b + v.c * v.c);
     return len;
 };
-void sub (vector& v1, const vector& v2, const vector& v3){
+
+void sub(vector &v1, const vector &v2, const vector &v3) {
     v1.a = v2.a + v3.a;
     v1.b = v2.b + v3.b;
     v1.c = v2.c + v3.c;
 };
-void norm (vector& v1){
+
+void norm(vector &v1) {
     double v = len(v1);
     v1.a = v1.a / v;
     v1.b = v1.b / v;
     v1.c = v1.c / v;
 };
-void scalar (vector& vector1, int n){
+
+void scalar(vector &vector1, int n) {
     vector1.a *= n;
     vector1.b *= n;
     vector1.c *= n;
 };
+//end of first task
 
-struct square {
-    double x,y;
+
+//Second task
+//Задача 2.
+//Да се дефинира структура, която определя правоъгълник по двете му страни. Да се дефинират функции,
+//които създават и извеждат правоъгълник, намират радиуса на описаната около правоъгълник окръжност, както и лицето и периметърана правоъгълник.
+//Да се напише програма, която създава правоъгълник по зададени страни. Извежда правоъгълника.
+//Намира и извежда радиуса на описаната около правоъгълника окръжност, периметъра и лицето му.
+struct rectangle {
+    double a, b;
 };
-void create(square& sq){
-    std::cin >> sq.x;
-    std::cin >> sq.y;
+
+void createRect(rectangle &rectangle1) {
+    std::cin >> rectangle1.a;
+    std::cin >> rectangle1.b;
 }
-void print(square& sq){
-    std::cout << sq.x;
-    std::cout << sq.y;
+
+
+void printRect(rectangle &rectangle1) {
+    std::cout << "rect a: " << rectangle1.a << "\n";
+    std::cout << "rect b: " << rectangle1.b << "\n";
+}
+
+void radiusRect(const rectangle rectangle1) {
+    double a = rectangle1.a;
+    double b = rectangle1.b;
+
+    double radius = sqrt(a * a + b * b);
+    std::cout << "radius rect: " << radius / 2 << "\n";
+}
+
+
+void areaRect(const rectangle rectangle1) {
+    std::cout << "area: " << rectangle1.a * rectangle1.b << "\n";
+    std::cout << "perimetar: " << rectangle1.a * 2 + 2 * rectangle1.b << "\n";
 }
 
 int main() {
@@ -56,7 +88,11 @@ int main() {
     print(v);
     norm(v);
     print(v);
-    std::cout << "len:" << len(v) << "\n";
+    rectangle rectangle1;
+    createRect(rectangle1);
+    printRect(rectangle1);
+    radiusRect(rectangle1);
+    areaRect(rectangle1);
     return 0;
 }
 
